a7c80261916620d9eba8437daf0f56ad2f1ef183
Nicola Fontana <ntd@entidi.it>
2016-10-13 12:02:25 +0200

    vimfx: correct history forward/backward shortcuts

--- home/.config/vimfx/config.js
+++ home/.config/vimfx/config.js
@@ -1,7 +1,7 @@
 let options = {
   "config_file_directory": "~/.config/vimfx",
-  "mode.normal.history_back": "K",
-  "mode.normal.history_forward": "J",
+  "mode.normal.history_back": "J",
+  "mode.normal.history_forward": "K",
   "mode.normal.tab_select_previous": "H",
   "mode.normal.tab_select_next": "L"
 }
