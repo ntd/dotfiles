1b61ddea3a90f104de16f5173a56e8a3d151f582
Nicola Fontana <ntd@entidi.it>
2020-06-02 18:42:48 +0200

    Add deployment script
    The new script will take care of merging the new GSettings.

--- README.md
+++ README.md
@@ -7,5 +7,4 @@ The files are maintained under a git repository handled by
 
     git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
     $HOME/.homesick/repos/homeshick/bin/homeshick -b clone ntd/dotfiles
-    $HOME/.homesick/repos/homeshick/bin/homeshick -f link
-    source $HOME/.bashrc
+    source $HOME/.homesick/repos/dotfiles/deploy.sh
---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Link dotfiles castle
+"$HOME"/.homesick/repos/homeshick/bin/homeshick -f link dotfiles
+
+# Set custom gsettings, if dconf and the specific program is installed
+dconf="$(which dconf)"
+if test -x "$dconf"; then
+    for dump in "$HOME"/.homesick/repos/dotfiles/home/.config/dconf/*.dump; do
+        path=$(sed -E 's|^.*/(.*).dump$|/\1/|; s|\.|/|g' <<< "$dump")
+        if test -n "$("$dconf" list $path)"; then
+            "$dconf" load < $dump
+        fi
+    done
+fi
+
+# Resource the new .bashrc
+source $HOME/.bashrc
